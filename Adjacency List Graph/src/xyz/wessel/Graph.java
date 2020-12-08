1d5e10e51f4180967e6996dfb51c5856d3d9e2bb
Thomas Wessel <turpleturtle12@gmail.com>
2020-12-07 19:42:39 -0800

    added some extra stuff

---
+++ Adjacency List Graph/src/xyz/wessel/Graph.java
@@ -0,0 +1,19 @@
+package xyz.wessel;
+
+import xyz.wessel.linkedList.LinkedList;
+
+/**
+ * This class creates a graph using the linked list class located in the linkedList package
+ */
+
+public class Graph {
+    //create the linked list that will hold other linked lists
+    LinkedList mainList = new LinkedList();
+
+    public Graph(){
+
+    }
+
+
+    
+}
--- Adjacency List Graph/src/xyz/wessel/linkedList/LinkedList.java
+++ Adjacency List Graph/src/xyz/wessel/linkedList/LinkedList.java
@@ -7,18 +7,18 @@ public class LinkedList {
 
     //this will be the head of the LinkedList
     private Node head;
-    private Node current;
+    private Node tail;
     private int numOfNodes;
 
 
     public LinkedList(){
         head = null;
-        current = null;
+        tail = null;
         numOfNodes = 0;
     }
 
     public LinkedList(Object dataObj){
-        //TODO: implement this
+        insert(dataObj);
     }
 
     public boolean isEmpty(){
@@ -36,16 +36,16 @@ public class LinkedList {
         //check to see if there is a node at the head
         if(head == null){
             //we should do a sanity check here
-            if(current == null){
+            if(tail == null){
                 head = toAdd;
-                current = head;
+                tail = head;
             }
             //this is an unsuported state, throw an exception
-            throw new IllegalStateException("Head/Current mismatch in Linklist");
+            throw new IllegalStateException("Head/tail mismatch in Linklist");
         }else{
             //the list should already be established
-            current.setNextNode(toAdd);
-            current = current.getNextNode();
+            tail.setNextNode(toAdd);
+            tail = tail.getNextNode();
         }
     }
 
@@ -55,28 +55,59 @@ public class LinkedList {
 
         //Check to see if a list exists already
         if(head == null){
-            //make sure that current is null too
-            if(current == null){
+            //make sure that tail is null too
+            if(tail == null){
                 head = toAdd;
-                current = head;
+                tail = head;
             }else{
                 //this should never happen but I want the program to fail fast if it does
-                throw new IllegalStateException("Head/Current mismatch in Linklist");
+                throw new IllegalStateException("Head/tail mismatch in Linklist");
             }
         }else{
-            //new head and keep current the same
+            //new head and keep tail the same
             toAdd.setNextNode(head);
             head = toAdd;
         }
     }
 
     public void deleteHead(){
-
+        if(head == null){
+            //do nothing, its already deleted
+        }else if(head.getNextNode() == null){
+            head = null;
+            tail = null; 
+        }else{
+            head = head.getNextNode();
+        }
     }
 
     public void delete(){
-
+        //this operation is a bit more tricky
+        if(numOfNodes <= 1){
+            //we can just call delete head so it removes the head
+            deleteHead();
+        }else{
+            //now we need to get the second to last node and keep a reference to it
+            Node second = head;
+            for(int i = 1; i < numOfNodes - 1; i++){
+                second = second.getNextNode();
+            }
+            //now delete the reference to the last node and move tail over
+            tail = second;
+            tail.setNextNode(null);
+        }
     }
+
+    //because we use an object for our data, this will not work most of the time
+    public void display(){
+        Node iterator = head;
+        while(iterator != tail){
+            System.out.println("[" + iterator.getDataObj().toString() + "]");
+        }
+        System.out.println("[" + iterator.getDataObj().toString() + "]");
+    }
+
+
 
 
 
